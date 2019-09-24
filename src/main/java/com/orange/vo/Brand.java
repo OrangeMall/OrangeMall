43c0be39e84104b2f84b425f783bfc51d35d2af8
sodawu <1091469478@qq.com>
2019-09-24 16:28:57 +0800

    修改商品品牌

---
+++ src/main/java/com/orange/vo/Brand.java
@@ -0,0 +1,18 @@
+package com.orange.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 商品品牌牌牌
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand {
+    private Integer bid;//品牌ID
+    private String bname;//品牌名称
+}
