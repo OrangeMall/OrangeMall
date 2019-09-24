126e9ea05609b85fd2189c3a87d494fde49d063d
qiuziqi1 <376682425@qq.com>
2019-09-24 09:29:21 +0800

    添加了实体类

---
+++ src/main/java/com/orange/vo/Brand.java
@@ -0,0 +1,18 @@
+package com.orange.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 商品品牌
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Brand {
+    private Integer bid;
+    private String bname;
+}
---
+++ src/main/java/com/orange/vo/Order_detail.java
@@ -0,0 +1,18 @@
+package com.orange.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 订单详情表
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order_detail {
+    private Integer did;//订单详情ID
+
+}
